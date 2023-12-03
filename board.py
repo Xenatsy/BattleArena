31dc30e23d9131abfcdf3c643226095b04b004cc
Alexander <39838284+Xenatsy@users.noreply.github.com>
2023-12-03 15:06:52 +0300

    change #2

--- board.py
+++ board.py
@@ -10,9 +10,19 @@ def print_board(board: list[list], t: str = "consolePresent"):
         for element in line:
             print(element[t], end=" ")
         print()
+ 
+
+def set_element(board: list[list], element: dict, x: int, y: int):
+    board[y][x] = element
+
+
+def get_element(board: list[list], x: int, y: int) -> dict:
+    return board[y][x]
+
 
 def distance(x1: int, y1: int, x2: int, y2: int) -> int:
     return abs(x1-x2) + abs(y1-y2)
+
 
 if __name__ == "__main__":
     print(distance(0,0,1,1))
