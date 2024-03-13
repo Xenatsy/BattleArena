1fddcfd9c371f26f688f5ec95b7418a4ed2ca682
Alexander <xenatsy@yandex.ru>
2024-03-13 03:25:08 +0300

    merging

---
+++ RecterCS/Unit.cs
@@ -0,0 +1,24 @@
+﻿class Unit
+{
+    private
+        string name;
+        int health;
+        int damage;
+        int range;
+    public
+        Unit(
+            string name,
+            int health,
+            int damage,
+            int range
+    ) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+        this.range = range;
+    }
+    public void getInfo()
+    {
+        Console.WriteLine($"{name} {health} {damage} {range}");
+    }
+}
